0da9266019286e291b5cf41496858e3a200f9aa0
radu crican <cricanr@gmail.com>
2018-09-23 17:12:47 +0200

    added SparseArrays coding challenge

---
+++ src/main/scala/hackerRank/algorithms/SparseArrays.scala
@@ -0,0 +1,42 @@
+package hackerRank.algorithms
+
+import java.io._
+
+object SparseArrays {
+
+  // Complete the matchingStrings function below.
+  def matchingStrings(strings: Array[String], queries: Array[String]): Array[Int] = {
+    queries.map { query =>
+      strings.map { string =>
+        if (string == query) 1 else 0
+      }.sum
+    }
+  }
+
+  def main(args: Array[String]) {
+    val stdin = scala.io.StdIn
+
+    val stringsCount = stdin.readLine.trim.toInt
+
+    val strings = Array.ofDim[String](stringsCount)
+
+    for (i <- 0 until stringsCount) {
+      val stringsItem = stdin.readLine
+      strings(i) = stringsItem
+    }
+
+    val queriesCount = stdin.readLine.trim.toInt
+
+    val queries = Array.ofDim[String](queriesCount)
+
+    for (i <- 0 until queriesCount) {
+      val queriesItem = stdin.readLine
+      queries(i) = queriesItem
+    }
+
+    val res = matchingStrings(strings, queries)
+
+    println(res.mkString("\n"))
+  }
+}
+
